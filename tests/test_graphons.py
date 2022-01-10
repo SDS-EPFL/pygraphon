2ffbeb69e36a252aaad085aae11dd8f499907f28
dufourc1 <dufourc115@gmail.com>
2022-01-10 14:01:40 +0100

    add test for graphon

---
+++ tests/test_graphons.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Test of graphons classes."""
+
+import unittest
+
+from pygraphon.core.graphons import Graphon
+
+
+class TestVersion(unittest.TestCase):
+    """Trivially test a version."""
+
+    def test_not_able_to_instantiate_abstract_class(self):
+        """Test the version is a string."""
+        with self.assertRaises(TypeError):
+            _ = Graphon()
