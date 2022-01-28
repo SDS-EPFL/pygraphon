9a9d123afde791d0ffbb2169762570998f0937ad
dufourc1 <dufourc115@gmail.com>
2022-01-28 15:29:37 +0100

    oracbwplugin works

--- src/pygraphon/matlab_functions/nethist.py
+++ src/pygraphon/matlab_functions/nethist.py
@@ -6,12 +6,87 @@ import scipy
 import scipy.sparse.linalg
 import scipy.special
 from scipy.spatial.distance import pdist, squareform
+from scipy import stats
 
 MATLAB_EPS = np.spacing(1)
 
 
 def upper_triangle_values(array):
     return array[np.triu_indices(array.shape[0])]
+
+
+def oracle_analysis_badnwidth(A: np.ndarray, type: str = "degs", alpha: float = 1) -> float:
+    c = min(4, np.sqrt(A.shape[0]) / 8)
+    h = oracbwplugin(A=A, c=c, type=type, alpha=alpha)
+    return h
+
+
+def oracbwplugin(
+    A: np.ndarray, c: float, type: str = "degs", alpha: float = 1
+) -> Tuple[float, float]:
+    """Oracle bandwidth plug-in estimtor for network histograms
+    h = oracbwplugin(A,c,type,alpha) returns a plug-in estimate
+    of the optimal histogram bandwidth (blockmodel community size) as a
+    function of the following inputs
+
+     Args:
+         A (np.ndarray): Adjacency matrix (must be a simple graph)
+         c (float): positive multiplier by which to estimate slope 1/- sqrt(n)
+         type (str, optional): Estimate slope from sorted vector ('degs' or 'eigs'). Defaults to "degs".
+         alpha (float, optional): Holder exponent. Defaults to 1.
+
+     Returns:
+         float: h
+
+
+     Examples:
+         >> h = oracbwplugin(A,3,'eigs',1); # returns h = 73.5910
+         >> h = oracbwplugin(A,3,'degs',1); # returns h = 74.1031
+    """
+
+    # input checks
+    if c <= 0:
+        raise ValueError("c must be positive")
+    if alpha != 1:
+        raise NotImplementedError("Currently only support alpha = 1")
+    A = checkAdjacencyMatrix(A)
+
+    n = A.shape[0]
+    midPt = np.arange(round(n / 2 - c * np.sqrt(n) - 1), round(n / 2 + c * np.sqrt(n)))
+    rhoHat = np.sum(A) / (n * (n - 1))
+    pseudo_inverse_rho_hat = 1 / rhoHat if rhoHat != 0 else 0
+
+    if type == "eigs":
+        mult, u = scipy.sparse.linalg.eigs(A, 1, which="LR")
+        u = u.real
+    elif type == "degs":
+        u = np.sum(A, axis=1)
+        mult = (u.T @ A @ u) / np.sum(u * u) ** 2
+    else:
+        raise NotImplementedError(f"Unknown input type: {type}")
+
+    u = np.sort(u.ravel())
+    uMid = u[midPt]
+    reg = stats.linregress(np.arange(1, len(uMid) + 1), uMid)
+    p = (reg[1], reg[0])
+    h = (
+        2 ** (alpha + 1)
+        * alpha
+        * mult ** 2
+        * (p[0] + p[1] * len(uMid) / 2) ** 2
+        * p[1] ** 2
+        * pseudo_inverse_rho_hat
+    ) ** (-1 / (2 * (alpha + 1)))
+    estMSqrd = (
+        2
+        * mult ** 2
+        * (p[0] + p[1] * len(uMid) / 2) ** 2
+        * p[1] ** 2
+        * pseudo_inverse_rho_hat ** 2
+        * (n + 1) ** 2
+    )
+    # MISEfhatBnd = estMSqrd * ((2 / np.sqrt(estMSqrd)) * (sampleSize * rhoHat) ** (-1 / 2) + 1 / n)
+    return h, estMSqrd
 
 
 def first_guess_blocks(A: np.ndarray, h: int, regParam: float) -> np.ndarray:
@@ -89,10 +164,7 @@ def nethist(A: np.ndarray, h: int = None) -> Tuple[List, int]:
     rhoHat = np.sum(A) / (n * (n - 1))
 
     # use data driven h
-    if h is None:
-        c = min(4, np.sqrt(n) / 8)
-        # fractional h before rounding
-        h, _ = oracbwplugin(A=A, c=c, type="degs", alpha=1)
+    h = h if h is not None else oracle_analysis_badnwidth(A)
 
     # why do we have minimum h = 2 ?
     h = max(2, min(n, np.round(h)))
@@ -104,79 +176,12 @@ def nethist(A: np.ndarray, h: int = None) -> Tuple[List, int]:
         h = h - 1
         lastGroupSize(n % h)
 
-    #idxInit = first_guess_blocks(A, h, regParam=rhoHat / 4)
+    # idxInit = first_guess_blocks(A, h, regParam=rhoHat / 4)
     idxInit = first_guess_blocks_python(A, h, rhoHat / 4)
 
     print(idxInit)
     idx, k = graphest_fastgreedy(A=A, hbar=h, inputLabelVec=idxInit)
     return idx, h
-
-
-def oracbwplugin(
-    A: np.ndarray, c: float, type: str = "degs", alpha: float = 1
-) -> Tuple[float, float]:
-    """Oracle bandwidth plug-in estimtor for network histograms
-    h = oracbwplugin(A,c,type,alpha) returns a plug-in estimate
-    of the optimal histogram bandwidth (blockmodel community size) as a
-    function of the following inputs
-
-     Args:
-         A (np.ndarray): Adjacency matrix (must be a simple graph)
-         c (float): positive multiplier by which to estumate slope 1/- sqrt(n)
-         type (str, optional): Estimate slope from sorted vector ('degs' or 'eigs'). Defaults to "degs".
-         alpha (float, optional): Holder exponent. Defaults to 1.
-
-     Returns:
-         float: h
-
-
-
-     Examples:
-         >> h = oracbwplugin(A,3,'eigs',1); # returns h = 73.5910
-
-         >> h = oracbwplugin(A,3,'degs',1); # returns h = 74.1031
-    """
-
-    # input checks
-    assert c > 0, "c must be positive"
-    A = checkAdjacencyMatrix(A)
-
-    n = A.shape[0]
-    midPt = np.arange(round(n / 2 - c * np.sqrt(n)), round(n / 2 + c * np.sqrt(n)))
-    # sampleSize = scipy.special.comb(n, 2)
-    rhoHat = np.sum(A) / (n * (n - 1))
-
-    if type == "eigs":
-        mult, u = scipy.sparse.linalg.eigs(A, 1)
-        u = u.real
-    elif type == "degs":
-        u = np.sum(A, axis=1)
-        mult = (u.T @ A @ u) / (u.T @ u) ** 2
-    else:
-        raise NotImplementedError(f"Unknown input type: {type}")
-
-    pseudo_inverse_rho_hat = 1 / rhoHat if rhoHat != 0 else 1
-    u = np.sort(u.ravel())
-    uMid = u[midPt[0] : midPt[-1]]
-    p = np.polyfit(range(1, len(u) + 1), u, 1)
-    h = (
-        2 ** (alpha + 1)
-        * alpha
-        * mult ** 2
-        * (p[1] + p[0] * len(uMid) / 2) ** 2
-        * p[1] ** 2
-        * pseudo_inverse_rho_hat
-    ) ** (-1 / (2 * (alpha + 1)))
-    estMSqrd = (
-        2
-        * mult ** 2
-        * (p[1] + p[1] * len(uMid) / 2) ** 2
-        * p[0] ** 2
-        * pseudo_inverse_rho_hat ** 2
-        * (n + 1) ** 2
-    )
-    # MISEfhatBnd = estMSqrd * ((2 / np.sqrt(estMSqrd)) * (sampleSize * rhoHat) ** (-1 / 2) + 1 / n)
-    return h, estMSqrd
 
 
 def graphest_fastgreedy(
