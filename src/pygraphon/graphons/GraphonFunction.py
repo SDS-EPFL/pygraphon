3d7be05400f754351bfba52bd2ddd4c7caf76e00
Charles Dufour <dufourc115@gmail.com>
2022-02-28 18:13:48 +0100

    add graphon defined only by function

---
+++ src/pygraphon/graphons/GraphonFunction.py
@@ -0,0 +1,19 @@
+from distutils.log import warn
+from typing import Callable
+from .GraphonAbstract import GraphonAbstract
+
+
+class Graphon(GraphonAbstract):
+
+    def __init__(self, function: Callable, scaled = False) -> None:
+
+        self.graphon_function = function
+        super().__init__(scaled)
+        
+
+    def graphon_function_builder(self) -> Callable:
+        return self.graphon_function
+    
+    def __add__(self, other):
+        function = lambda x, y: 0.5*self.graphon_function(x, y) +  0.5* other.graphon_function(x, y)
+        return Graphon(function)
