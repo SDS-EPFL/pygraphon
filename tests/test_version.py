96a57bace6f7871123363afdd58efa6ccd44d9cc
Charles Dufour <dufourc115@gmail.com>
2022-03-07 17:59:06 +0100

    add init on test

--- tests/test_version.py
+++ tests/test_version.py
@@ -10,6 +10,9 @@ from src.pygraphon.version import get_version
 class TestVersion(unittest.TestCase):
     """Trivially test a version."""
 
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+
     def test_version_type(self):
         """Test the version is a string.
 
@@ -17,3 +20,4 @@ class TestVersion(unittest.TestCase):
         """
         version = get_version()
         self.assertIsInstance(version, str)
+
