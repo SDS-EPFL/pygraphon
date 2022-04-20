06f7865e51a9f0c8ae07e56b0d9e89df79480736
Charles Dufour <dufourc115@gmail.com>
2022-04-20 12:45:34 +0200

    black files

--- src/pygraphon/plots/plots_graphon.py
+++ src/pygraphon/plots/plots_graphon.py
@@ -3,16 +3,16 @@ from typing import Tuple
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.pyplot import Axes, Figure
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits import axes_grid1
 
 from pygraphon.graphons.GraphonFunction import Graphon
 from pygraphon.graphons.StepGraphon import StepGraphon
-from mpl_toolkits import axes_grid1
+
 
 def add_colorbar(im, aspect=20, pad_fraction=0.5, **kwargs):
     """Add a vertical color bar to an image plot."""
     divider = axes_grid1.make_axes_locatable(im.axes)
-    width = axes_grid1.axes_size.AxesY(im.axes, aspect=1./aspect)
+    width = axes_grid1.axes_size.AxesY(im.axes, aspect=1.0 / aspect)
     pad = axes_grid1.axes_size.Fraction(pad_fraction, width)
     current_ax = plt.gca()
     cax = divider.append_axes("right", size=width, pad=pad)
@@ -40,8 +40,9 @@ def plot_sample(
     ax: Axes = None,
     colorbar=False,
     integrate_to_1: bool = False,
-) ->  Tuple[Figure, Axes]:
-    
+) -> Tuple[Figure, Axes]:
+    """Plot the graphon."""
+
     x1, x2 = np.linspace(0, 1, resolution, endpoint=False), np.linspace(
         0, 1, resolution, endpoint=False
     )
