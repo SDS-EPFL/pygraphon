edb016d6461649e206e47317e585a8fd4432e97c
dufourc1 <dufourc115@gmail.com>
2022-10-04 13:28:59 +0200

    update test on utils

--- tests/test_utils/test_matrix.py
+++ tests/test_utils/test_matrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from pygraphon.utils.utils_matrix import permute_matrix
+from pygraphon.utils.utils_matrix import check_symmetric, permute_matrix
 
 
 def test_permute_matrix_simple():
@@ -14,3 +14,8 @@ def test_permute_matrix_complex():
     a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     b = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
     assert np.allclose(permute_matrix(a, (2, 1, 0)), b)
+
+
+def test_check_symmetric():
+    a = np.array([[1, 2], [2, 1]])
+    assert check_symmetric(a)
