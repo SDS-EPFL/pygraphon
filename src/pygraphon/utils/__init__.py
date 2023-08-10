008bb4cfd17ae64479c4eef7f865b1f7bb0851d0
ArthurVerdeyme <arthur.verdeyme@gmail.com>
2023-08-10 17:22:20 +0200

    change init and api accordingly

--- src/pygraphon/utils/__init__.py
+++ src/pygraphon/utils/__init__.py
@@ -7,7 +7,7 @@ from .utils_graph import (
     get_adjacency_matrix_from_graph,
 )
 from .utils_graphon import compute_areas_histogram
-from .utils_maths import EPS, bic, generate_all_permutations, log_likelihood
+from .utils_maths import EPS, bic, generate_all_permutations, log_likelihood, aic, elbow_point
 from .utils_matrix import (
     bound_away_from_one_and_zero_arrays,
     check_symmetric,
